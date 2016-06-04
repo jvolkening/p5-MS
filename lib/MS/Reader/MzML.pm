009a42dc46b18bbf47a7fa2f00a2cb59eba3e9e4
jvolkening <jdv@base2bio.com>
2016-06-03 23:26:08 -0500

    updated documentation

--- lib/MS/Reader/MzML.pm
+++ lib/MS/Reader/MzML.pm
@@ -120,7 +120,7 @@ sub next_spectrum {
 
 sub find_by_time {
 
-    my ($self, $ret, $ms_level) = @_;
+    my ($self, $rt, $ms_level) = @_;
 
     # lazy load
     if (! defined $self->{rt_index}) {
@@ -129,11 +129,14 @@ sub find_by_time {
 
     my @sorted = @{ $self->{rt_index} };
 
+    croak "Retention time out of bounds"
+        if ($rt < 0 || $rt > $self->{rt_index}->[-1]->[1]);
+
     # binary search
     my ($lower, $upper) = (0, $#sorted);
     while ($lower != $upper) {
         my $mid = int( ($lower+$upper)/2 );
-        ($lower,$upper) = $ret < $sorted[$mid]->[1]
+        ($lower,$upper) = $rt < $sorted[$mid]->[1]
             ? ( $lower , $mid   )
             : ( $mid+1 , $upper );
     }
@@ -198,30 +201,34 @@ sub get_tic {
 
     my ($self, $force) = @_;
 
-    my $idx = $self->{index}->{chromatogram}->{TIC};
-    if (! $force && defined $self->{offsets}->{chromatogram}->[$idx]) {
+    if (my $chrom = first { exists $_->{cvParam}->{&MS_TOTAL_ION_CURRENT_CHROMATOGRAM} }
+      @{ $self->{mzML}->{run}->{chromatogramList}->{chromatogram} } ) {
+        my $idx = $self->{index}->{chromatogram}->{ $chrom->{id} };
         my $offset = $self->{offsets}->{chromatogram}->[$idx];
+        croak "No offset found for chromatogram"
+            if (! defined $offset);
         my $to_read = $self->{lengths}->{chromatogram}->[$idx];
-#
+
         my $el   = $self->_read_element($offset,$to_read);
         return MS::Reader::MzML::Chromatogram->new(xml => $el);
     }
+
     return MS::Reader::MzML::Chromatogram->new(type => 'tic', raw => $self);
 
 }
 
 sub get_xic {
 
-    my ($self,%args) = @_;
-    return MS::Reader::MzML::Chromatogram->new(type => 'xic',raw => $self,%args);
+    my ($self, %args) = @_;
+    return MS::Reader::MzML::Chromatogram->new(type => 'xic',raw => $self, %args);
 
 }
 
 sub get_bpc {
 
-    my ($self) = @_;
+    my ($self, $force) = @_;
 
-    if (my $chrom = first { defined $_->{cvParam}->{&BASEPEAK_CHROMATOGRAM} }
+    if (my $chrom = first { defined $_->{cvParam}->{&MS_BASEPEAK_CHROMATOGRAM} }
       @{ $self->{mzML}->{run}->{chromatogramList}->{chromatogram} } ) {
         my $idx = $self->{index}->{chromatogram}->{ $chrom->{id} };
         my $offset = $self->{offsets}->{chromatogram}->[$idx];
@@ -251,6 +258,186 @@ MS::Reader::MzML - A simple but complete mzML parser
 
     use MS::Reader::MzML;
 
-    my $p = MS::Reader::MzML->new('run.mzML');
+    my $run = MS::Reader::MzML->new('run.mzML');
+
+    while (my $spectrum = $run->next_spectrum) {
+       
+        # only want MS1
+        next if ($spectrum->ms_level > 1);
+
+        my $rt = $spectrum->rt;
+        # see MS::Reader::MzML::Spectrum and MS::Spectrum for all available
+        # methods
+
+    }
+
+    $spectrum = $run->fetch_spectrum(0);  # first spectrum
+    $spectrum = $run->find_by_time(1500); # in seconds
+
+
+=head1 DESCRIPTION
+
+C<MS::Reader::MzML> is a parser for the HUPO PSI standard mzML format for raw
+mass spectrometry data. It aims to provide complete access to the data
+contents while not being overburdened by detailed class infrastructure.
+Convenience methods are provided for accessing commonly used data. Users who
+want to extract data not accessible through the available methods should
+examine the data structure of the parsed object. The C<dump()> method of
+L<MS::Reader::XML>, from which this class inherits, provides an easy method of
+doing so.
+
+=head1 INHERITANCE
+
+C<MS::Reader::MzML> is a subclass of L<MS::Reader::XML>, which in turn
+inherits from L<MS::Reader>, and inherits the methods of these parental
+classes. Please see the documentation for those classes for details of
+available methods not detailed below.
+
+=head1 METHODS
+
+=over 4
+
+=item B<next_spectrum>
+
+    while (my $s = $run->next_spectrum) { # do something }
+
+Returns an C<MS::Reader::MzML::Spectrum> object representing the next spectrum
+in the file, or C<undef> if the end of records has been reached. Typically
+used to iterate over each spectrum in the run.
+
+=item B<fetch_spectrum> <index>
+
+    my $s = $run->fetch_spectrum($idx);
+
+Returns an C<MS::Reader::MzML::Spectrum> object representing the spectrum at
+index <index>. Indices are zero-based. Throws an exception if the index is out
+of range.
+
+=item B<find_by_time> <retention time>
+
+    my $nearest = $run->find_by_time($rt);
+
+Returns an C<MS::Reader::MzML::Spectrum> object representing the nearest
+spectrum with retention time (IN SECONDS) equal to or greater than that given.
+Throws an exception if the given retention time is out of range.
+
+NOTE: The first time this method is called, the spectral indices are sorted by
+retention time for subsequent access. This can be a bit slow. The retention
+time index is saved and subsequent calls should be relatively quick. This is
+done because the mzML specification doesn't guarantee that the spectra are
+ordered by RT (even though they invariably are).
+
+=item B<n_spectra>
+
+    my $n = $run->n_spectra;
+
+Returns the number of spectra present in the file.
+
+=item B<n_spectra>
+
+    my $n = $run->n_spectra;
+
+Returns the number of spectra present in the file.
+
+=item B<get_tic> [<force>]
+
+    my $tic = $run->get_tic;
+
+Returns an C<MS::Reader::MzML::Chromatogram> object containing the total ion
+current chromatogram for the run. By default, first searches the chromatogram
+list to see if a TIC is already defined, and returns it if so. Otherwise,
+walks the MS1 spectra and calculates the TIC. Takes a single optional boolean
+argument which, if true, forces recalculation of the TIC even if one exists in
+the file.
+
+=item B<get_bpc> [<force>]
+
+    my $tic = $run->get_bpc;
+
+Returns an C<MS::Reader::MzML::Chromatogram> object containing the base peak
+chromatogram for the run. By default, first searches the chromatogram
+list to see if a BPC is already defined, and returns it if so. Otherwise,
+walks the MS1 spectra and calculates the BPC. Takes a single optional boolean
+argument which, if true, forces recalculation of the BPC even if one exists in
+the file.
+
+=item B<get_xic> [%args]
+
+    my $xic = $run->get_xic(
+        mz      => 200.0037,
+        err_ppm => 10,
+        rt      => 1245,
+        rt_win  => 120,
+    );
+
+Returns an C<MS::Reader::MzML::Chromatogram> object containing an extracted
+ion chromatogram for the run. Valid arguments are:
+
+=over 4
+
+=item mz
+
+The m/z value to extract
+
+=item err_ppm
+
+The tolerance for extraction (in ppm)
+
+=item rt
+
+The center of the retention time window to extract (IN SECONDS). If not given,
+the entire run will be searched.
+
+=item rt_win
+
+The window on either size of the target retention time to search (IN SECONDS).
+
+=item charge
+
+The expected charge of the species at this m/z. If specified (along with
+C<iso_steps>) a search for the peaks within the isotopic envelope C<iso_steps>
+steps above and below the target m/z will be included in the returned ion
+flows. 
+
+=item iso_steps
+
+The number of isotopic steps to search above and below the target m/z for
+inclusion of the isotopic envelope. C<charge> must also be specified or this
+value will be ignored.
+
+=back
+
+=item B<id>
+
+Returns the ID of the run as specified in the C<<mzML>> element.
+
+=back
+
+=head1 CAVEATS AND BUGS
+
+The API is in alpha stage and is not guaranteed to be stable.
+
+Please reports bugs to the author.
+
+=head1 AUTHOR
+
+Jeremy Volkening <jdv *at* base2bio.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2015-2016 Jeremy Volkening
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
