75af7ffae964320f834bbe2eec64039fa7ca2d1c
jvolkening <jdv@base2bio.com>
2016-06-06 09:22:42 -0500

    POD updates

--- lib/MS/Reader/PepXML.pm
+++ lib/MS/Reader/PepXML.pm
@@ -79,6 +79,13 @@ sub next_result {
 
 }
 
+sub fetch_result {
+
+    my ($self, $idx, %args) = @_;
+    return $self->fetch_record('spectrum_query', $idx, %args);
+
+}
+
 sub next_list_result {
 
     my ($self, $list_id) = @_;
@@ -89,10 +96,10 @@ sub next_list_result {
 
 }
 
-sub result_lists {
+sub n_lists {
 
     my ($self) = @_;
-    return (0..scalar(@{$self->{msms_run_summary}})-1);
+    return scalar @{$self->{msms_run_summary}};
 
 }
 
@@ -109,14 +116,157 @@ sub goto_list {
 
 __END__
 
+=pod
+
+=encoding UTF-8
+
 =head1 NAME
 
-MS::Reader::PepXML - A simple but complete PepXML parser
+MS::Reader::PepXML - A simple but complete pepXML parser
 
 =head1 SYNOPSIS
 
     use MS::Reader::PepXML;
 
-    my $p = MS::Reader::PepXML->new('search.pep.xml');
+    my $search = MS::Reader::PepXML->new('search.pep.xml');
+
+    # for single search files
+
+    while (my $result = $search->next_result($_) {
+        # $result is an MS::Reader::PepXML::Result object
+    }
+
+    # for multi-search files
+
+    my $n = $search->n_lists;
+
+    for (1..$n) {
+        
+        $self->goto_list($_);
+        while (my $result = $search->next_list_result($_) {
+            # $result is an MS::Reader::PepXML::Result object
+        }
+       
+    }
+
+
+=head1 DESCRIPTION
+
+C<MS::Reader::PepXML> is a parser for the pepXML file format for spectral
+search results. It aims to provide complete access to the data contents while
+not being overburdened by detailed class infrastructure.  Convenience methods
+are provided for accessing commonly used data. Users who want to extract data
+not accessible through the available methods should examine the data structure
+of the parsed object. The C<dump()> method of L<MS::Reader::XML>, from which
+this class inherits, provides an easy method of doing so.
+
+=head1 INHERITANCE
+
+C<MS::Reader::PepXML> is a subclass of L<MS::Reader::XML>, which in turn
+inherits from L<MS::Reader>, and inherits the methods of these parental
+classes. Please see the documentation for those classes for details of
+available methods not detailed below.
+
+=head1 METHODS
+
+=head2 new
+
+    my $run = MS::Reader::PepXML->new( $fn,
+        use_cache => 0,
+        paranoid  => 0,
+    );
+
+Takes an input filename (required) and optional argument hash and returns an
+C<MS::Reader::PepXML> object. This constructor is inherited directly from
+L<MS::Reader>. Available options include:
+
+=over
+
+=item * use_cache — cache fetched records in memory for repeat access
+(default: FALSE)
+
+=item * paranoid — when loading index from disk, recalculates MD5 checksum
+each time to make sure raw file hasn't changed. This adds (typically) a few
+seconds to load times. By default, only file size and mtime are checked.
+
+=back
+
+=head2 next_result
+
+    while (my $s = $search->next_result) { # do something }
+
+Returns an C<MS::Reader::PepXML::Result> object representing the next result
+in the file (pepXML element <<spectrum_query>>, or C<undef> if the end of
+records has been reached. Iterates over ALL results in the file, which may not
+be what you want for a multi-search file (see C<next_list_result> below).
+
+=head2 fetch_result
+
+    my $s = $search->fetch_result($idx);
+
+Takes a single argument (zero-based spectrum index) and returns an
+C<MS::Reader::PepXML::Result> object representing the record at that index.
+Throws an exception if the index is out of range.
+
+=head2 n_lists
+
+    my $n = $search->n_lists;
+
+Returns the number of result lists (pepXML <<msms_run_summary>> elements) in
+the file. If this number is greater than one, individual lists can be iterated
+over using C<goto_list> and C<next_list_result>.
+
+=head2 goto_list
+
+    $search->goto_list($idx);
+
+Takes a single argument (zero-based list index) and sets the record pointer to
+the first result from that list.
+
+=head2 next_list_result
+
+    my $r = $search->next_list_result($idx);
+
+Takes a single argument (zero-based list index) and returns the next result
+from that list, searching from the current record pointer, or undef if the end
+of the list has been reached.
+
+=head1 CAVEATS AND BUGS
+
+The API is in alpha stage and is not guaranteed to be stable.
+
+Please reports bugs or feature requests through the issue tracker at
+L<https://github.com/jvolkening/p5-MS/issues>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<InSilicoSpectro>
+
+=item * L<PepXML::Parser>
+
+=back
+
+=head1 AUTHOR
+
+Jeremy Volkening <jdv@base2bio.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2015-2016 Jeremy Volkening
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
