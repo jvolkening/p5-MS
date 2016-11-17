bf2e685a8a1fc9cf874a5916e0f2cdf144f39fea
jvolkening <jdv@base2bio.com>
2016-11-17 10:23:19 -0600

    POD updates to MS::Search::DB

--- lib/MS/Search/DB.pm
+++ lib/MS/Search/DB.pm
@@ -174,5 +174,180 @@ sub add_crap {
 
 }
 
-
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+MS::Search::DB - A class to facilitate construction of MS/MS protein search databases
+
+=head1 SYNOPSIS
+
+    use MS::Search::DB;
+
+    my $db = MS::Search::DB;
+
+    # add sequences from various sources
+
+    $db->add_from_file("/path/to/proteins.faa");
+    $db->add_from_url("http://foo.org/proteomes/XYZ.faa");
+    $db->add_from_source(
+        source   => 'uniprot',
+        taxid    => '12345',
+        ref_only => 1,
+    )
+
+    # add contaminant sequences from cRAP
+    $db->add_crap();
+
+    # generate decoy sequences
+    $db->add_decoys(
+        type => 'reverse',
+        prefix => 'DECOY_',
+    );
+
+    # write to fh (default STDOUT)
+    $db->write(
+        fh        => $fh,
+        randomize => 1,
+    );
+
+
+=head1 DESCRIPTION
+
+C<MS::Search::DB> is intended to facilitate easy construction of MS/MS protein
+search databases from various sources. It includes methods for fetching
+protein sequence data, adding common contaminant sequences, adding decoy
+sequences, and saving the database to disk.
+
+=head1 METHODS
+
+=head2 new
+
+    my $db = MS::Search::DB->new();
+
+    #or, initialize directly from a file
+    my $db = MS::Search::DB->new('/path/to/proteins.faa');
+
+Create a new C<MS::Search::DB> object. A single optional argument pointing to
+a FASTA file is accepted, which will be loaded into the initial database.
+
+=head2 add_from_file
+
+    $db->add_from_file('/path/to/proteins.faa');
+
+Takes a single required argument (path to a protein FASTA file) and loads it
+into the database. Returns the number of sequences added.
+
+=head2 add_from_url
+
+    $db->add_from_url('http://somedb.org/proteomes/XYZ.faa');
+
+Takes a single required argument (URL referencing a FASTA file) and loads it
+into the database. Returns the number of sequences added.
+
+=head2 add_from_source
+
+    $db->add_from_source(
+        source => 'uniprot',
+        # plugin-specific arguments
+    );
+
+Fetch data using an MS::Search::DB::Source plugin (specified via the 'source'
+argument). These plugins facilitate searching common sources of protein
+sequence data, such as NCBI or Uniprot. Please see the documentation for each
+individual plugin (under the C<MS::Search::DB::Source::> namespace) for
+details of the arguments each one accepts.
+
+=head2 add_crap
+
+    $db->add_crap();
+    $db->add_crap($url);
+
+Downloads common contaminant sequences and adds them to the database. By
+default, downloads the "common Repository of Adventitious Proteins", aka
+"cRAP", from GPM. An optional URL can be provided to fetch from another
+source.
+
+=head2 add_decoys
+
+    $db->add_decoys(
+        type => 'reverse',
+        prefix => 'DECOY_',
+    );
+
+Generates a set of decoy sequences according to the arguments provided and
+adds them to the database. One decoy will be added for each protein in the
+original database. Possible arguments include:
+
+=over
+
+=item * type — how to generate the decoy sequences. Either 'reverse' or
+'shuffle'. (default: reverse)
+
+=item * prefix — the prefix to be added to each decoy ID. (default: "DECOY_")
+
+=back
+
+Note that the order in which this method is called matters. Only sequences
+that have already been added to the database before it is called will be used
+for decoy generation. 
+
+=head2 write
+
+    $db->write(
+        fh => $fh,
+        randomize => 1,
+    );
+
+Write database to disk as FASTA file. Possible arguments include:
+
+=over
+
+=item * fh — filehandle to write to (default: STDOUT)
+
+=item * randomize — whether to randomly shuffle sequences before writing
+(default: 0)
+
+=back
+
+=head1 CAVEATS AND BUGS
+
+Please reports bugs or feature requests through the issue tracker at
+L<https://github.com/jvolkening/p5-MS/issues>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<InSilicoSpectro>
+
+=back
+
+=head1 AUTHOR
+
+Jeremy Volkening <jdv@base2bio.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2015-2016 Jeremy Volkening
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
--- lib/MS/Search/DB/Source/uniprot.pm
+++ lib/MS/Search/DB/Source/uniprot.pm
@@ -36,9 +36,9 @@ sub fetch_fh {
 
         close $rdr;
 
-        my $ref_only = $self->{reference_only} ? 'yes' : 'no';
+        my $ref_only = $self->{ref_only} ? 'yes' : 'no';
         my $top_node = $self->{taxid} // die "No taxon specified\n";
-        die "Taxon must be integer ID\n" if ($top_node =~ /\D/);
+        die "Taxon must be NCBI integer ID\n" if ($top_node =~ /\D/);
 
         my $list_url = "http://www.uniprot.org/proteomes/?query=reference:$ref_only+taxonomy:$top_node&format=list";
 
@@ -66,3 +66,74 @@ sub fetch_fh {
 
 1;
         
+__END__
+
+=head1 NAME
+
+MS::DB::Search::Source::uniprot - interface for Uniprot downloads
+
+=head1 SYNOPSIS
+
+    use MS::DB::Search;
+
+    my $db = MS::DB::Search->new();
+
+    $db->add_from_source(
+        source   => 'uniprot',
+        taxid    => '12345',
+        ref_only => 1,
+    );
+
+=head1 DESCRIPTION
+
+C<MS::DB::Search::Source::uniprot> provides an interface to the Uniprot
+database for fetching proteomes based on taxonomic ID. It is not intended to
+be called directly, but rather used via the 'source' argument to the
+L<MS::DB::Search> C<add_from_source> method.
+
+=head1 ARGUMENTS
+
+The following arguments should be provided to the L<MS::DB::Search>
+C<add_from_source> method call:
+
+=over 4
+
+=item B<source> <plugin>
+
+Should be 'uniprot'
+
+=item B<taxid> <id>
+
+A numeric NCBI taxonomic ID for the organim of interest (required)
+
+=item B<ref_only> <bool>
+
+Whether to only fetch reference proteomes (default: 0)
+
+=head1 CAVEATS AND BUGS
+
+Please report bugs to the author.
+
+=head1 AUTHOR
+
+Jeremy Volkening <jdv@base2bio.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2016 Jeremy Volkening
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
